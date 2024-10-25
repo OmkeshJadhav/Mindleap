cf3280096436c4b451b475dab9c71a8729cfba6a
Omkesh Jadhav <iamomkeshjadhav@gmail.com>
2024-10-25 23:13:29 +0530

    Assets added

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-red-500">
-      Hello world!
-    </h1>
+      <h1 className="text-3xl font-bold font-secondary underline text-red-500">
+        Hello world!
+      </h1>
     </>
   )
 }
